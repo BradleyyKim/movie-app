c589656aa62696b8d479a324f28e0261defe4d31
김민영 <win930323@gmail.com>
2022-03-08 14:30:44 +0900

    correcting...

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />}></Route>
       </Routes>
     </Router>
   );
